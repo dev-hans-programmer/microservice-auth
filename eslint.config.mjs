ded43f51f87c28517f81b5c305bdebb63d942773
Hasan Ali <hans1998ali@gmail.com>
2025-09-03 01:16:21 +0530

    Added eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig(
+    eslint.configs.recommended,
+    tseslint.configs.recommendedTypeChecked,
+    {
+        ignores: ['eslint.config.mjs', 'node_modules', 'dist'],
+    },
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+        rules: {
+            'no-console': 'warn',
+            'dot-notation': 'warn',
+        },
+    },
+);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
+/* eslint-disable no-console */
 console.log(`Hello from node ${process.version}`);
 console.log('Hello');
 
 function getName() {
     console.log('Hello Name');
+    const user = {
+        name: 'Hasan',
+    };
+    console.log(user.name);
 }
 console.log(getName());
