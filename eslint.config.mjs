398ed8b9604d64d8f3021f0f274546b0588d481a
Hasan Ali <hans1998ali@gmail.com>
2025-09-03 01:31:13 +0530

    Tab width 2 for formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,21 +3,21 @@ import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 
 export default defineConfig(
-    eslint.configs.recommended,
-    tseslint.configs.recommendedTypeChecked,
-    {
-        ignores: ['eslint.config.mjs', 'node_modules', 'dist'],
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  {
+    ignores: ['eslint.config.mjs', 'node_modules', 'dist'],
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
-    {
-        languageOptions: {
-            parserOptions: {
-                projectService: true,
-                tsconfigRootDir: import.meta.dirname,
-            },
-        },
-        rules: {
-            'no-console': 'warn',
-            'dot-notation': 'warn',
-        },
+    rules: {
+      'no-console': 'warn',
+      'dot-notation': 'warn',
     },
+  },
 );
--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,11 @@ console.log(`Hello from node ${process.version}`);
 console.log('Hello');
 
 function getName() {
-    console.log('Hello Name');
-    const user = {
-        name: 'Hasan',
-        age: 26,
-    };
-    console.log(user.name);
+  console.log('Hello Name');
+  const user = {
+    name: 'Hasan',
+    age: 26,
+  };
+  console.log(user.name);
 }
 console.log(getName());
