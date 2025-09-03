9404b11c7f80a9c4dd2df37362aa9d4badb4f209
Hasan Ali <hans1998ali@gmail.com>
2025-09-04 00:50:28 +0530

    Added error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,36 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import createHttpError, { HttpError } from 'http-errors';
+import logger from './config/logger';
+import { StatusCodes } from 'http-status-codes';
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello server' });
+});
+
+app.get('/error-route', () => {
+  throw createHttpError(
+    StatusCodes.UNAUTHORIZED,
+    'You cannot access this route',
+  );
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: HttpError, req: Request, res: Response, _next: NextFunction) => {
+  logger.error(err.message);
+  const statusCode = err.statusCode || 500;
+
+  res.status(statusCode).json({
+    errors: [
+      {
+        type: err.name,
+        path: '',
+        location: '',
+        msg: err.message,
+      },
+    ],
+  });
 });
 
 export default app;
